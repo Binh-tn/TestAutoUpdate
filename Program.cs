4308e54c5140d97846ce1f4d46215f148273ee87
binhtn <binhtn@softdreams.vn>
2024-10-13 20:03:31 +0700

    update

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using AutoUpdaterDotNET;
+using System;
+using System.Windows.Forms;
 
 namespace TestAutoUpdate
 {
     internal class Program
     {
+        private const string UPDATE_FOLDER = "update";
+
         static void Main(string[] args)
         {
+            //AutoUpdater.Synchronous = false;
+            //AutoUpdater.RunUpdateAsAdmin = false;
+
+            AutoUpdater.SetOwner(args);
+            //AutoUpdater.Synchronous = true;
+            AutoUpdater.DownloadPath = UPDATE_FOLDER;
+            AutoUpdater.CheckForUpdateEvent += AutoUpdaterOnCheckForUpdateEvent;
+
             AutoUpdater.Start("https://binh-tn.github.io/TestAutoUpdate/update.xml");
+        }
+
+        private static void AutoUpdaterOnCheckForUpdateEvent(UpdateInfoEventArgs args)
+        {
+            if (args.IsUpdateAvailable)
+            {
+                if (AutoUpdater.DownloadUpdate(args))
+                {
+                    Application.ExitThread();
+                }
+            }
         }
     }
 }
